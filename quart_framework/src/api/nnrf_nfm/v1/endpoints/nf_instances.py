294ebb50fd387c11f80e12fa20f4df4b497425fc
lvdund <lvdund@gmail.com>
2023-02-08 20:07:59 +0700

    nrf

--- quart_framework/src/api/nnrf_nfm/v1/endpoints/nf_instances.py
+++ quart_framework/src/api/nnrf_nfm/v1/endpoints/nf_instances.py
@@ -1,7 +1,10 @@
 from app import app, request
+from src.env.config import settings
 from quart import jsonify
 from src.crud.crud_nf_instance import *
-
+from src.schemas.HeartBeatTimer import listNF_heartBeatTimer
+from src.schemas.NFStatus import NFStatus
+import time
 
 @app.put("/nnrf-nfm/v1/nf-instances/<nfInstanceId>")
 async def nf_register(nfInstanceId):
@@ -23,6 +26,18 @@ async def nf_read(nfInstanceId):
 async def nf_update(nfInstanceId):
     return jsonify(modify_nf_instance(nfInstanceId=nfInstanceId, update_values=await request.get_json()))
 
+# NF Heart-Beat
+def nf_heart_beat():
+    while True:
+        time.sleep(settings.hear_beat_timer)
+        current_time = time.time()
+        for nf in listNF_heartBeatTimer:
+            if (current_time-nf.updateTime)>5.5:
+                nf.nfStatus = NFStatus.SUSPENDED
+                modify_nf_instance(nfInstanceId=nf.nfInstanceId, update_values={"nfStatus": "SUSPENDED"})
+
 @app.get("/nnrf-nfm/v1/nf-instances")
 async def nf_discovery():
-    
+    return jsonify(dc_get_nf_instance(find_value=await request.get_json()))
+
+
--- quart_framework/src/crud/crud_nf_instance.py
+++ quart_framework/src/crud/crud_nf_instance.py
@@ -1,5 +1,8 @@
 from src.database.database import NfProfile, urilist
-
+from src.schemas.HeartBeatTimer import listNF_heartBeatTimer, Nf_heartBeat
+from src.schemas.common.NfInstanceId import NfInstanceId
+from src.schemas.NFStatus import NFStatus
+import time
 # from bson.objectid import ObjectId
 
 # from Services import environment
@@ -8,6 +11,16 @@ def get_nf_instance(nfInstanceId):
     try:
         nf_profile = NfProfile.find_one({"nfInstanceId": nfInstanceId})
         return nf_profile
+    except:
+        return None
+    
+def dc_get_nf_instance(find_value):
+    try:
+        a = []
+        list_nf_profile = NfProfile.find(find_value)
+        for i in list_nf_profile:
+            a.append(i["nfType"])
+        return a
     except:
         return None
 
@@ -23,6 +36,7 @@ def create_nf_instance(nfInstanceId, nf_profile):
             nf_profile["nfInstanceId"] = nfInstanceId
         NfProfile.insert_one(nf_profile)
         print("Register NF")
+        listNF_heartBeatTimer.append(Nf_heartBeat(nfInstanceId=NfInstanceId(nfInstanceId), nfStatus=NFStatus.REGISTERED, updateTime=time.time()))
         return 200
     except:
         print("Cannot insert to nfProfile collection")
@@ -34,6 +48,10 @@ def delete_nf_instance(nfInstanceId):
         return 400
     try:
         NfProfile.delete_one({"nfInstanceId": nfInstanceId})
+        for i in range(len(listNF_heartBeatTimer)):
+            if listNF_heartBeatTimer[i].nfInstanceId == nfInstanceId:
+                del listNF_heartBeatTimer[i]
+        
         print("Deleted: " + str(nfInstanceId))
         return 200
     except:
@@ -45,8 +63,13 @@ def modify_nf_instance(nfInstanceId, update_values):
         print("NF does not exist")
         return 400
     try:
+        update_values.update({"nfStatus": "REGISTERED"})
         new_values = { "$set": update_values}
         NfProfile.update_one({"nfInstanceId": nfInstanceId}, new_values)
+        for i in range(len(listNF_heartBeatTimer)):
+            if listNF_heartBeatTimer[i].nfInstanceId == nfInstanceId:
+                listNF_heartBeatTimer[i].updateTime = time.time()
+        
         return 200
     except:
         print("Cannot update")
